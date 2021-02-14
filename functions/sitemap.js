5120ebe2980c5e0a8cb3ae0ca973ec941a033581
Richard Beattie <richard.beattie@aol.co.uk>
2021-02-14 14:25:06 +0000

    Fix xml formatting

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -1,14 +1,13 @@
 exports.handler = async (event) => {
   const domain = "https://r-bt.com";
 
-  const sitemap = `
-  <?xml version="1.0" encoding="UTF-8"?>
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
           <loc>${domain}/</loc>
           <changefreq>weekly</changefreq>
       </url>
-    </urlset> 
+    </urlset>
   `;
 
   return {
