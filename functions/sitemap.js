048d7c8567ae89d5e855a0b390062e17b0294805
Richard Beattie <richard.beattie@aol.co.uk>
2021-02-14 14:21:25 +0000

    Netlify function to create sitemap

---
+++ functions/sitemap.js
@@ -0,0 +1,19 @@
+exports.handler = async (event) => {
+  const domain = "https://r-bt.com";
+
+  const sitemap = `
+  <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+          <loc>${domain}/</loc>
+          <changefreq>weekly</changefreq>
+      </url>
+    </urlset> 
+  `;
+
+  return {
+    statusCode: 200,
+    contentType: "text/xml",
+    body: sitemap,
+  };
+};
