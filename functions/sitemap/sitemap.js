11cc37c70b3a3226c4ec2a70a7a1267a7c6f41d6
Richard Beattie <richard.beattie@aol.co.uk>
2021-02-14 18:32:13 +0000

    Print files in dir

--- functions/sitemap/sitemap.js
+++ functions/sitemap/sitemap.js
@@ -1,4 +1,5 @@
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
+const fs = require("fs");
 //Change 123
 
 const NOTION_API = "https://notion-api.r-bt.workers.dev/v1";
@@ -8,46 +9,52 @@ const getNotionTable = (id) => {
 };
 
 exports.handler = async (event) => {
-  const domain = "https://r-bt.com";
+  // const domain = "https://r-bt.com";
 
-  const static = ["/", "/about", "/learning", "/blog", "/links"];
+  // const static = ["/", "/about", "/learning", "/blog", "/links"];
 
-  const learningPages = (
-    await getNotionTable("489999d5f3d240c0a4fedd9de71cbb6f")
-  )
-    .filter((item) => item.Status === "Visible" && item.slug)
-    .map((item) => item.slug);
+  // const learningPages = (
+  //   await getNotionTable("489999d5f3d240c0a4fedd9de71cbb6f")
+  // )
+  //   .filter((item) => item.Status === "Visible" && item.slug)
+  //   .map((item) => item.slug);
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${static
-        .map(
-          (page) =>
-            `
-          <url>
-            <loc>${domain}${page}</loc>
-            <changefreq>weekly</changefreq>
-          </url>
-        `
-        )
-        .join("")}
-        ${learningPages
-          .map(
-            (page) =>
-              `
-            <url>
-              <loc>${domain}/${page}</loc>
-              <changefreq>weekly</changefreq>
-            </url>
-          `
-          )
-          .join("")}
-    </urlset>
-  `;
+  // const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  //   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  //     ${static
+  //       .map(
+  //         (page) =>
+  //           `
+  //         <url>
+  //           <loc>${domain}${page}</loc>
+  //           <changefreq>weekly</changefreq>
+  //         </url>
+  //       `
+  //       )
+  //       .join("")}
+  //       ${learningPages
+  //         .map(
+  //           (page) =>
+  //             `
+  //           <url>
+  //             <loc>${domain}/${page}</loc>
+  //             <changefreq>weekly</changefreq>
+  //           </url>
+  //         `
+  //         )
+  //         .join("")}
+  //   </urlset>
+  // `;
+
+  const sitemap = await new Promise((resolve) => {
+    fs.readdir(__dirname, (err, files) => {
+      resolve(files.join(","));
+    });
+  });
 
   return {
     statusCode: 200,
-    contentType: "text/xml",
+    contentType: "text/html",
     body: sitemap,
   };
 };
