3fd2541657369aac65c20cf231ab92e4e65216b9
Richard Beattie <richard.beattie@aol.co.uk>
2021-02-14 14:25:06 +0000

    Fix xml formatting

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -3,12 +3,12 @@ exports.handler = async (event) => {
 
   const sitemap = `
   <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-          <loc>${domain}/</loc>
-          <changefreq>weekly</changefreq>
-      </url>
-    </urlset> 
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>${domain}/</loc>
+        <changefreq>weekly</changefreq>
+    </url>
+  </urlset>
   `;
 
   return {
