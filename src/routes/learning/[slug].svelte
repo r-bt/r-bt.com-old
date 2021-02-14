d9b45ed356a6544843a7c33a23507d417bc663a3
Richard Beattie <richard.beattie@aol.co.uk>
2021-02-14 13:43:17 +0000

    Add Twitter, Facebook, etc meta tags

--- src/routes/learning/[slug].svelte
+++ src/routes/learning/[slug].svelte
@@ -8,17 +8,22 @@
   import { onMount } from "svelte";
   import { Notion } from "notion-in-svelte";
   import "prismjs/themes/prism-okaidia.css";
+  import { stores } from "@sapper/app";
   // Stores
   import { posts, getPosts } from "./posts";
   // COMPONENTS
   import Profile from "../../components/Profile.svelte";
   import config from "../../config";
+  import profileImg from "images/profile.jpg";
+
+  const { page } = stores();
 
   export let slug;
   let blocks;
+  let post;
 
   $: if ($posts && process.browser) {
-    const post = $posts.find((post) => post.slug === slug);
+    post = $posts.find((post) => post.slug === slug);
     if (post) {
       fetch(`${config.NOTION_API}/page/${post.id}`).then(async (res) => {
         const post = await res.json();
@@ -37,7 +42,28 @@
 </script>
 
 <svelte:head>
-  <title>R-BT - Learning</title>
+  {#if post}
+    <title>{post.Name}</title>
+    <meta name="twitter:title" content={post.Name} />
+    <meta property="og:title" content={post.Name} />
+    {#if post.description}
+      <meta name="description" content={post.description} />
+      <meta name="twitter:description" content={post.description} />
+      <meta property="og:description" content={post.description} />
+    {/if}
+  {:else}
+    <title>R-BT – Learning</title>
+  {/if}
+
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@r_bt_" />
+  <meta name="twitter:creator" content="@r_bt_" />
+  <meta name="twitter:image" content="https://r-bt.com{profileImg}" />
+
+  <meta property="og:type" content="article" />
+  <meta property="og:url" content="https://r-bt.com{$page.path}" />
+  <meta property="og:image" content="https://r-bt.com{profileImg}" />
+  <meta property="og:site_name" content="R-BT Blog" />
 </svelte:head>
 
 <Profile title="Learning in Public" />
