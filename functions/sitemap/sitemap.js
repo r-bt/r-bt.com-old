7e6b4338a30eb4f1123507d6cf15195dba7dea67
Richard Beattie <richard.beattie@aol.co.uk>
2021-02-14 18:20:47 +0000

    Just testing anything

--- functions/sitemap/sitemap.js
+++ functions/sitemap/sitemap.js
@@ -1,10 +1,10 @@
-const axios = require("axios");
+const fetch = require("node-fetch");
 //Change
 
 const NOTION_API = "https://notion-api.r-bt.workers.dev/v1";
 
 const getNotionTable = (id) => {
-  return axios.get(`${NOTION_API}/table/${id}`).then((resp) => resp.data);
+  return fetch(`${NOTION_API}/table/${id}`).then((resp) => resp.json());
 };
 
 exports.handler = async (event) => {
