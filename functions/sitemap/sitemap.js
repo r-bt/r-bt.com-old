c0f5ab7df799f0bcb92ddadade4b4993bd958427
Richard Beattie <richard.beattie@aol.co.uk>
2021-02-19 18:13:11 +0000

    Console.log fetch

--- functions/sitemap/sitemap.js
+++ functions/sitemap/sitemap.js
@@ -5,46 +5,48 @@ const fs = require("fs");
 exports.handler = async (event) => {
   const NOTION_API = "https://notion-api.r-bt.workers.dev/v1";
 
-  const getNotionTable = (id) => {
-    return fetch(`${NOTION_API}/table/${id}`).then((resp) => resp.json());
-  };
+  console.log({ fetch });
 
-  const domain = "https://r-bt.com";
+  // const getNotionTable = (id) => {
+  //   return fetch(`${NOTION_API}/table/${id}`).then((resp) => resp.json());
+  // };
 
-  const static = ["/", "/about", "/learning", "/blog", "/links"];
+  // const domain = "https://r-bt.com";
 
-  const learningPages = (
-    await getNotionTable("489999d5f3d240c0a4fedd9de71cbb6f")
-  )
-    .filter((item) => item.Status === "Visible" && item.slug)
-    .map((item) => item.slug);
+  // const static = ["/", "/about", "/learning", "/blog", "/links"];
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${static
-        .map(
-          (page) =>
-            `
-          <url>
-            <loc>${domain}${page}</loc>
-            <changefreq>weekly</changefreq>
-          </url>
-        `
-        )
-        .join("")}
-        ${learningPages
-          .map(
-            (page) =>
-              `
-            <url>
-              <loc>${domain}/${page}</loc>
-              <changefreq>weekly</changefreq>
-            </url>
-          `
-          )
-          .join("")}
-    </urlset>
-  `;
+  // const learningPages = (
+  //   await getNotionTable("489999d5f3d240c0a4fedd9de71cbb6f")
+  // )
+  //   .filter((item) => item.Status === "Visible" && item.slug)
+  //   .map((item) => item.slug);
+
+  // const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  //   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  //     ${static
+  //       .map(
+  //         (page) =>
+  //           `
+  //         <url>
+  //           <loc>${domain}${page}</loc>
+  //           <changefreq>weekly</changefreq>
+  //         </url>
+  //       `
+  //       )
+  //       .join("")}
+  //       ${learningPages
+  //         .map(
+  //           (page) =>
+  //             `
+  //           <url>
+  //             <loc>${domain}/${page}</loc>
+  //             <changefreq>weekly</changefreq>
+  //           </url>
+  //         `
+  //         )
+  //         .join("")}
+  //   </urlset>
+  // `;
 
   const sitemap2 = await new Promise((resolve) => {
     fs.readdir(`${__dirname}/node_modules`, (err, files) => {
