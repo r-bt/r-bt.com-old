42a6c375d63771aec36ce34ce4c654d6ad8754f2
Richard Beattie <richard.beattie@aol.co.uk>
2021-02-19 18:24:24 +0000

    Use notion to create sitemap

--- functions/sitemap/sitemap.js
+++ functions/sitemap/sitemap.js
@@ -1,76 +1,52 @@
+const { filter } = require("compression");
 const fetch = require("node-fetch");
-const fs = require("fs");
-//Change 123
-
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
 
 exports.handler = async (event) => {
   const NOTION_API = "https://notion-api.r-bt.workers.dev/v1";
   const id = "489999d5f3d240c0a4fedd9de71cbb6f";
 
-  console.log({ fetch });
+  let pages = [];
+  try {
+    pages = await fetch(`${NOTION_API}/table/${id}`, {
+      headers: { Accept: "application/json" },
+    }).then((response) => response.json());
+  } catch (e) {
+    return { statusCode: 422, body: String(e) };
+  }
 
-  console.log(`${NOTION_API}/table/${id}`);
+  const domain = "https://r-bt.com";
+  const static = ["/", "/about", "/learning", "/blog", "/links"];
 
-  return fetch(`${NOTION_API}/table/${id}`, {
-    headers: { Accept: "application/json" },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log({ data });
-      return {
-        statusCode: 200,
-        body: "Something....",
-      };
-    })
-    .catch((error) => ({ statusCode: 422, body: String(error) }));
+  const filteredPages = pages
+    .filter((item) => item.Status === "Visible" && item.slug)
+    .map((item) => item.slug);
 
-  // const getNotionTable = (id) => {
-  //   return fetch(`${NOTION_API}/table/${id}`).then((resp) => resp.json());
-  // };
-
-  // const domain = "https://r-bt.com";
-
-  // const static = ["/", "/about", "/learning", "/blog", "/links"];
-
-  // const learningPages = (
-  //   await getNotionTable("489999d5f3d240c0a4fedd9de71cbb6f")
-  // )
-  //   .filter((item) => item.Status === "Visible" && item.slug)
-  //   .map((item) => item.slug);
-
-  // const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  //   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  //     ${static
-  //       .map(
-  //         (page) =>
-  //           `
-  //         <url>
-  //           <loc>${domain}${page}</loc>
-  //           <changefreq>weekly</changefreq>
-  //         </url>
-  //       `
-  //       )
-  //       .join("")}
-  //       ${learningPages
-  //         .map(
-  //           (page) =>
-  //             `
-  //           <url>
-  //             <loc>${domain}/${page}</loc>
-  //             <changefreq>weekly</changefreq>
-  //           </url>
-  //         `
-  //         )
-  //         .join("")}
-  //   </urlset>
-  // `;
-
-  const sitemap2 = await new Promise((resolve) => {
-    fs.readdir(`${__dirname}/node_modules`, (err, files) => {
-      resolve(files.join(","));
-    });
-  });
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${static
+        .map(
+          (page) =>
+            `
+          <url>
+            <loc>${domain}${page}</loc>
+            <changefreq>weekly</changefreq>
+          </url>
+        `
+        )
+        .join("")}
+        ${filteredPages
+          .map(
+            (page) =>
+              `
+            <url>
+              <loc>${domain}/${page}</loc>
+              <changefreq>weekly</changefreq>
+            </url>
+          `
+          )
+          .join("")}
+    </urlset>
+  `;
 
   return {
     statusCode: 200,
