6f2b41381d4297e67661d7251a32a156dfbd05f1
Richard Beattie <richard.beattie@aol.co.uk>
2021-02-19 17:37:40 +0000

    Uncomment correct sitemap code

--- functions/sitemap/sitemap.js
+++ functions/sitemap/sitemap.js
@@ -1,6 +1,4 @@
 const fetch = require("node-fetch");
-const fs = require("fs");
-//Change 123
 
 exports.handler = async (event) => {
   const NOTION_API = "https://notion-api.r-bt.workers.dev/v1";
@@ -9,48 +7,42 @@ exports.handler = async (event) => {
     return fetch(`${NOTION_API}/table/${id}`).then((resp) => resp.json());
   };
 
-  // const domain = "https://r-bt.com";
+  const domain = "https://r-bt.com";
 
-  // const static = ["/", "/about", "/learning", "/blog", "/links"];
+  const static = ["/", "/about", "/learning", "/blog", "/links"];
 
-  // const learningPages = (
-  //   await getNotionTable("489999d5f3d240c0a4fedd9de71cbb6f")
-  // )
-  //   .filter((item) => item.Status === "Visible" && item.slug)
-  //   .map((item) => item.slug);
+  const learningPages = (
+    await getNotionTable("489999d5f3d240c0a4fedd9de71cbb6f")
+  )
+    .filter((item) => item.Status === "Visible" && item.slug)
+    .map((item) => item.slug);
 
-  // const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  //   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  //     ${static
-  //       .map(
-  //         (page) =>
-  //           `
-  //         <url>
-  //           <loc>${domain}${page}</loc>
-  //           <changefreq>weekly</changefreq>
-  //         </url>
-  //       `
-  //       )
-  //       .join("")}
-  //       ${learningPages
-  //         .map(
-  //           (page) =>
-  //             `
-  //           <url>
-  //             <loc>${domain}/${page}</loc>
-  //             <changefreq>weekly</changefreq>
-  //           </url>
-  //         `
-  //         )
-  //         .join("")}
-  //   </urlset>
-  // `;
-
-  const sitemap = await new Promise((resolve) => {
-    fs.readdir(`${__dirname}`, (err, files) => {
-      resolve(files.join(","));
-    });
-  });
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${static
+        .map(
+          (page) =>
+            `
+          <url>
+            <loc>${domain}${page}</loc>
+            <changefreq>weekly</changefreq>
+          </url>
+        `
+        )
+        .join("")}
+        ${learningPages
+          .map(
+            (page) =>
+              `
+            <url>
+              <loc>${domain}/${page}</loc>
+              <changefreq>weekly</changefreq>
+            </url>
+          `
+          )
+          .join("")}
+    </urlset>
+  `;
 
   return {
     statusCode: 200,
